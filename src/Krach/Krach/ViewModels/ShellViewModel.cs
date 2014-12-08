f455a62d484e730bea0e50908479a37dfd2a6022
k-nut <knut@k-nut.eu>
2014-12-08 19:27:41 +0100

    Add some example interactivity

--- src/Krach/Krach/ViewModels/ShellViewModel.cs
+++ src/Krach/Krach/ViewModels/ShellViewModel.cs
@@ -1,46 +1,72 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Windows.Documents;
 using Caliburn.Micro;
-using Krach.Views;
 using OxyPlot;
-using OxyPlot.Series;
 
 namespace Krach.ViewModels
 {
-    [Export(typeof(IShell))]
-    public class ShellViewModel : Screen, IShell
+  [Export(typeof (IShell))]
+  public class ShellViewModel : Screen, IShell
+  {
+    private readonly string description;
+    private string firstSelected = "Blue";
+    private string secondSelected = "Transparent";
+    private string thirdSelected = "Transparent";
+
+    private readonly List<DataPoint> points = new List<DataPoint>
     {
-      private string description;
+      new DataPoint(0, 4),
+      new DataPoint(10, 13),
+      new DataPoint(20, 15),
+      new DataPoint(30, 16),
+      new DataPoint(40, 12),
+      new DataPoint(50, 12)
+    };
 
-      private List<DataPoint> points = new List<DataPoint>
-                              {
-                                  new DataPoint(0, 4),
-                                  new DataPoint(10, 13),
-                                  new DataPoint(20, 15),
-                                  new DataPoint(30, 16),
-                                  new DataPoint(40, 12),
-                                  new DataPoint(50, 12)
-                              }; 
-
-
-        public void Close()
-        {
-            this.TryClose();
-        }
-
-      public ShellViewModel()
-      {
-        description = "hier steht ein TExt";
-        NotifyOfPropertyChange(() => Description);
-      }
-
-      public string Description {
-        get { return description; }
-      }
-
-      public List<DataPoint> Points { get { return points; }}
-
-
+    public void SelectNext()
+    {
+      firstSelected = "Transparent";
+      secondSelected = "Blue";
+      NotifyOfPropertyChange(() => FirstSelected);
+      NotifyOfPropertyChange(() => SecondSelected);
     }
+
+
+    public ShellViewModel()
+    {
+      description = "hier steht ein TExt";
+      NotifyOfPropertyChange(() => Description);
+    }
+
+    public string Description
+    {
+      get { return description; }
+    }
+
+    public string SecondSelected
+    {
+      get { return secondSelected; }
+    }
+
+    public string FirstSelected
+    {
+      get { return firstSelected; }
+    }
+
+    public string ThirdSelectd
+    {
+      get { return thirdSelected; }
+    }
+
+
+    public List<DataPoint> Points
+    {
+      get { return points; }
+    }
+
+    public void Close()
+    {
+      TryClose();
+    }
+  }
 }
