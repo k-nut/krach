1704bac16fb4a3d9180afa3f0bb42a6907dfc75b
Knut Hühne <knut@k-nut.eu>
2014-12-13 13:44:44 +0100

    Remove moq and  test real things

--- src/KrachConnect/KrachConnect.Tests/NoiseMeasurmentAddViewModelFixture.cs
+++ src/KrachConnect/KrachConnect.Tests/NoiseMeasurmentAddViewModelFixture.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KrachConnect.DomainModelService;
-using Moq;
 using NUnit.Framework;
 
 namespace KrachConnect.Tests
@@ -14,11 +13,9 @@ namespace KrachConnect.Tests
     [Test]
     public void Adding_A_New_NoiseMeasurement_To_MeasuringPoint()
     {
-      var measuringPointMock = new Mock<MeasuringPoint>();
-      measuringPointMock.Setup(mpm => mpm.Name).Returns("Test");
-//      var mp = new MeasuringPoint{Name="Test"};
-      var nm = new NoiseMeasurement {MeasuringPoint = measuringPointMock.Object};
-      Assert.AreEqual(nm.MeasuringPoint, measuringPointMock.Object.Name);
+      var mp = new MeasuringPoint{Name="Test"};
+      var nm = new NoiseMeasurement {MeasuringPoint = mp};
+      Assert.AreEqual(nm.MeasuringPoint, mp);
       Assert.AreEqual(nm.MeasuringPoint.Name, "Test");
     }
   }
