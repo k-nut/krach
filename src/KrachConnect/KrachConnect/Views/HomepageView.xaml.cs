f4bca30485c6703bea8fc0312087c34fe419c5f3
ConnyRichter <constanze.richter@yahoo.de>
2015-01-05 12:47:40 +0100

    Startseite Design verbessert

--- src/KrachConnect/KrachConnect/Views/HomepageView.xaml.cs
+++ src/KrachConnect/KrachConnect/Views/HomepageView.xaml.cs
@@ -24,5 +24,10 @@ namespace KrachConnect.Views
         {
             InitializeComponent();
         }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
