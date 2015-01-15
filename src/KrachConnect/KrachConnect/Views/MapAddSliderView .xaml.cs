37e428be07224b0e328abf42aef3406d6c372cf0
SmartPart <smarterarty@gmail.com>
2015-01-15 14:03:31 +0100

    Revert "Remove old fiels"
    This reverts commit 949448b61f7a945370732447b3e0276bdd9498e8.

---
+++ src/KrachConnect/KrachConnect/Views/MapAddSliderView .xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace KrachConnect.Views
+{
+  /// <summary>
+  ///   Interaction logic for MapAddView.xaml
+  /// </summary>
+  public partial class MapAddSliderView : UserControl
+  {
+    public MapAddSliderView()
+    {
+      InitializeComponent();
+    }
+  }
+}
