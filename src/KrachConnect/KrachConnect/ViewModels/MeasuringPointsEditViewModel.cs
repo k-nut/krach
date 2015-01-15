7948c0ba807c2ee8d4fbe389b516af3f7f576a3d
SmartPart <smarterarty@gmail.com>
2015-01-15 13:57:32 +0100

    Messp. erstellen überarbeitet - Dialog fehlt

--- src/KrachConnect/KrachConnect/ViewModels/MeasuringPointsEditViewModel.cs
+++ src/KrachConnect/KrachConnect/ViewModels/MeasuringPointsEditViewModel.cs
@@ -1,83 +1,85 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using Caliburn.Micro;
 using KrachConnect.DomainModelService;
 
 namespace KrachConnect.ViewModels
 {
-  internal class MeasuringPointsEditViewModel : PropertyChangedBase
-  {
-    private readonly NoiseRepository repository;
-    private ObservableCollection<MeasuringPointViewModel> measuringPointViewModels;
-    private MeasuringPointViewModel selectedMeasuringPoint;
-
-    public MeasuringPointsEditViewModel(NoiseRepository repository)
+    internal class MeasuringPointsEditViewModel : PropertyChangedBase
     {
-      this.repository = repository;
-      MeasuringPoints = new ObservableCollection<MeasuringPointViewModel>(repository.MeasuringPointViewModels);
-      SelectedMeasuringPoint = MeasuringPoints.First();
-    }
+        private readonly NoiseRepository repository;
+        private ObservableCollection<MeasuringPointViewModel> measuringPointViewModels;
+        private MeasuringPointViewModel selectedMeasuringPoint;
 
-    public ObservableCollection<MeasuringPointViewModel> MeasuringPoints
-    {
-      get { return measuringPointViewModels; }
-      set
-      {
-        measuringPointViewModels = value;
-        NotifyOfPropertyChange(() => MeasuringPoints);
-      }
-    }
+        public MeasuringPointsEditViewModel(NoiseRepository repository)
+        {
+            this.repository = repository;
+            MeasuringPoints = new ObservableCollection<MeasuringPointViewModel>(repository.MeasuringPointViewModels);
+            SelectedMeasuringPoint = MeasuringPoints.First();
+        }
 
-    public MeasuringPointViewModel SelectedMeasuringPoint
-    {
-      get { return selectedMeasuringPoint; }
-      set
-      {
-        selectedMeasuringPoint = value;
-        selectedMeasuringPoint.IsSelected = true;
-        NotifyOfPropertyChange(() => SelectedMeasuringPoint);
-      }
-    }
+        public ObservableCollection<MeasuringPointViewModel> MeasuringPoints
+        {
+            get { return measuringPointViewModels; }
+            set
+            {
+                measuringPointViewModels = value;
+                NotifyOfPropertyChange(() => MeasuringPoints);
+            }
+        }
 
-    public void SaveToHub()
-    {
-      repository.Save();
-    }
+        public MeasuringPointViewModel SelectedMeasuringPoint
+        {
+            get { return selectedMeasuringPoint; }
+            set
+            {
+                selectedMeasuringPoint = value;
+                selectedMeasuringPoint.IsSelected = true;
+                NotifyOfPropertyChange(() => SelectedMeasuringPoint);
+            }
+        }
 
-    public void ToggleArchivation(object dataContext)
-    {
-      var measuringPoint = (MeasuringPointViewModel) dataContext;
-      measuringPoint.IsArchived = !measuringPoint.IsArchived;
-      repository.Save();
-    }
+        public void SaveToHub()
+        {
+            repository.Save();
+        }
 
-    public void ChangeSelectedMeasuringPoint(object dataContext)
-    {
-      SelectedMeasuringPoint.IsSelected = false;
-      var measuringPointViewModel = (MeasuringPointViewModel) dataContext;
-      SelectedMeasuringPoint = measuringPointViewModel;
-    }
+        public void ToggleArchivation(object dataContext)
+        {
+            var measuringPoint = (MeasuringPointViewModel) dataContext;
+            measuringPoint.IsArchived = !measuringPoint.IsArchived;
+            repository.Save();
+        }
 
-    public void AddNewMeasuringPoint(object xPosition, object yPosition)
-    {
-      var x = (int) xPosition;
-      var y = (int) yPosition;
-      var newPosition = new NoiseMapPosition
-      {
-        XPosition = x - 10,
-        YPosition = y - 10
-        // 10 is a magic number
-        // because our points are width and height 20px
-        // in order to position the points in the center
-        // we simply subtract half the size (10)
-      };
-      ChangeSelectedMeasuringPoint(new MeasuringPointViewModel(new MeasuringPoint
-      {
-        Position = newPosition,
-        Name = "Neuer Messpunkt"
-      }));
-      MeasuringPoints.Add(SelectedMeasuringPoint);
-      repository.MeasuringPoints.Add(SelectedMeasuringPoint.Model);
+        public void ChangeSelectedMeasuringPoint(object dataContext)
+        {
+            SelectedMeasuringPoint.IsSelected = false;
+            var measuringPointViewModel = (MeasuringPointViewModel) dataContext;
+            SelectedMeasuringPoint = measuringPointViewModel;
+        }
+
+        public void AddNewMeasuringPoint(object xPosition, object yPosition)
+        {
+            var x = (int) xPosition;
+            var y = (int) yPosition;
+            var newPosition = new NoiseMapPosition
+            {
+                XPosition = x - 10,
+                YPosition = y - 10
+                // 10 is a magic number
+                // because our points are width and height 20px
+                // in order to position the points in the center
+                // we simply subtract half the size (10)
+            };
+            ChangeSelectedMeasuringPoint(new MeasuringPointViewModel(new MeasuringPoint
+            {
+                Position = newPosition,
+                Name = "Neuer Messpunkt"
+            }));
+            MeasuringPoints.Add(SelectedMeasuringPoint);
+            repository.MeasuringPoints.Add(SelectedMeasuringPoint.Model);
+        }
     }
-  }
 }
