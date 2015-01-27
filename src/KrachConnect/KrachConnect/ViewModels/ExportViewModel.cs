12e1d864dcc839500254e8b21d05b69d2fd5cbc9
k-nut <knut@k-nut.eu>
2015-01-27 15:31:18 +0100

    dont use multiple worksheets in excel export

--- src/KrachConnect/KrachConnect/ViewModels/ExportViewModel.cs
+++ src/KrachConnect/KrachConnect/ViewModels/ExportViewModel.cs
@@ -90,32 +90,28 @@ namespace KrachConnect.ViewModels
         myStream = saveFileDialog1.OpenFile();
         using (var package = new ExcelPackage(myStream))
         {
-          var groupedNoiseMeasurements = _selectedNoiseMeasurementViewModels.Where(nm => nm.MeasuringPoint != null)// TODO: If the data is valid this cannot happen
-            .GroupBy(nm => nm.MeasuringPoint);
-          foreach (var group in groupedNoiseMeasurements)
+          var selectedNoiseMeasuremenets =
+            _selectedNoiseMeasurementViewModels.Where(nm => nm.MeasuringPoint != null).ToList();// TODO: If the data is valid this cannot happen
+          ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Messwerte");
+          worksheet.Cells[1, 1].Value = "Messpunkt";
+          worksheet.Cells[1, 2].Value = "Datum";
+          worksheet.Cells[1, 3].Value = "Minimalwert";
+          worksheet.Cells[1, 4].Value = "Durchschnittswert";
+          worksheet.Cells[1, 5].Value = "Maximalwert";
+
+
+          for (var i = 0; i < selectedNoiseMeasuremenets.Count(); i++)
           {
-            var name = group.Key.Name;
-            while (package.Workbook.Worksheets.Any(ws => ws.Name == name))
-            {
-              name = "(anderer) " + name;
-            }
-            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(name);
-            worksheet.Cells[1, 1].Value = "Datum";
-            worksheet.Cells[1, 2].Value = "Minimalwert";
-            worksheet.Cells[1, 3].Value = "Durchschnittswert";
-            worksheet.Cells[1, 4].Value = "Maximalwert";
-            var list = group.ToList();
-            for (var i = 0; i < list.Count(); i++)
-            {
-              var nm = list[i];
-              worksheet.Cells[i + 2, 1].Value = nm.MeasurementDate;
-              worksheet.Cells[i + 2, 1].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
-              worksheet.Cells[i + 2, 2].Value = nm.MinValue;
-              worksheet.Cells[i + 2, 3].Value = nm.AverageValue;
-              worksheet.Cells[i + 2, 4].Value = nm.MaxValue;
-            }
-            worksheet.Cells.AutoFitColumns(0);
+            var nm = selectedNoiseMeasuremenets[i];
+            worksheet.Cells[i + 2, 1].Value = nm.MeasuringPoint.Name;
+            worksheet.Cells[i + 2, 2].Value = nm.MeasurementDate;
+            worksheet.Cells[i + 2, 2].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.FullDateTimePattern;
+            worksheet.Cells[i + 2, 3].Value = nm.MinValue;
+            worksheet.Cells[i + 2, 4].Value = nm.AverageValue;
+            worksheet.Cells[i + 2, 5].Value = nm.MaxValue;
           }
+          worksheet.Cells.AutoFitColumns(0);
+
           package.Save();
         }
         myStream.Close();
